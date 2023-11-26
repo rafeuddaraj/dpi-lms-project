057aeae5a060166008b7af114922caccecd31f23
Rafe Uddaraj <rafeuddaraj2@gmail.com>
2023-11-26 21:56:55 +0600

    authentication slice done

--- src/features/auth/authApi.js
+++ src/features/auth/authApi.js
@@ -1,7 +1,45 @@
 import { apiSlice } from "../apiSlice/apiSlice";
+import { userLoggedIn } from "./authSlice";
 
 export const authApi = apiSlice.injectEndpoints({
-    endpoints:builder=>({})
+    endpoints: builder => ({
+        login: builder.mutation({
+            query: (data) => ({
+                url: '/login',
+                method: "POST",
+                body: data
+            }),
+            async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+                try {
+                    const data = await queryFulfilled
+                    const user = data?.data
+                    const accessToken = user?.accessToken
+                    localStorage.setItem('auth', accessToken)
+                    dispatch(userLoggedIn(user))
+                } catch {
+                    //
+                }
+            }
+        }),
+        register: builder.mutation({
+            query: (data) => ({
+                url: '/register',
+                method: "POST",
+                body: data
+            }),
+            async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+                try {
+                    const data = await queryFulfilled
+                    const user = data?.data
+                    const accessToken = user?.accessToken
+                    localStorage.setItem('auth', accessToken)
+                    dispatch(userLoggedIn(user))
+                } catch {
+                    //
+                }
+            }
+        }),
+    })
 })
 
-export const {} = authApi
+export const { useLoginMutation, useRegisterMutation } = authApi
